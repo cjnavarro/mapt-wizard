cb2da81641eb43cddf2938385bd28d78c64fca55
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-18 22:26:57 -0400

    more clen up

--- src/main/java/com/mapt/MaptApplication.java
+++ src/main/java/com/mapt/MaptApplication.java
@@ -32,9 +32,8 @@ public class MaptApplication extends Application<MaptConfiguration> {
 
     	final JdbiFactory factory = new JdbiFactory();
     	final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
-    	jdbi.setSqlLogger(SqlLogger.logException());
     	
-    	 final UserDAO userDAO = jdbi.onDemand(UserDAO.class);
+    	final UserDAO userDAO = jdbi.onDemand(UserDAO.class);
     	
         final HelloResource resource = new HelloResource(
                 configuration.getTemplate(),
