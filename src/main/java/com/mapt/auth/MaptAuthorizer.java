eb0cfe0dd5b6a55d92a8465c55a85892fbb4230b
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-19 21:26:55 -0400

    let other non admin through for public calls

--- src/main/java/com/mapt/auth/MaptAuthorizer.java
+++ src/main/java/com/mapt/auth/MaptAuthorizer.java
@@ -1,14 +1,18 @@
 package com.mapt.auth;
 
+import javax.ws.rs.container.PreMatching;
+
 import com.mapt.core.User;
 
 import io.dropwizard.auth.Authorizer;
 
+@PreMatching
 public class MaptAuthorizer implements Authorizer<User>
 {
     @Override
     public boolean authorize(User user, String role)
     {
-        return user.getName().equals("Chris Navarro") && role.equals("ADMIN");
+        return (user.getName().equals("Chris Navarro") && role.equals("ADMIN")) 
+        		|| (user !=null && !role.equals("ADMIN"));
     }
 }
