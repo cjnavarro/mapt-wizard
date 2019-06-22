440261934760be0967129877ccae61ab35747cfd
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-22 12:25:59 -0400

    add ui

---
+++ src/main/resources/assets/precache-manifest.c6ce34f6dae539985ee4470d591caa69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e10e9f759b2def83a7ca4f1fbd1ec1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01bc6576d35066c00fb8",
+    "url": "/static/css/2.56d85521.chunk.css"
+  },
+  {
+    "revision": "bd7d1dbb26e1430879af",
+    "url": "/static/css/main.73244a65.chunk.css"
+  },
+  {
+    "revision": "01bc6576d35066c00fb8",
+    "url": "/static/js/2.8bf38496.chunk.js"
+  },
+  {
+    "revision": "bd7d1dbb26e1430879af",
+    "url": "/static/js/main.dce5b051.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
