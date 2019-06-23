49fd45087a5b611c943a08ba309fc18e040be3f5
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-23 09:49:38 -0400

    add endpoint to get current principle

--- src/main/java/com/mapt/resources/UserResource.java
+++ src/main/java/com/mapt/resources/UserResource.java
@@ -13,7 +13,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import java.util.List;
 
@@ -45,6 +47,15 @@ public class UserResource
     public List<User> getAllUsers()
     {
         return userDAO.findAll();
+    }
+    
+    @GET
+    @Timed
+    @Path("/me")
+    @PermitAll
+    public User getMe(@Context SecurityContext context)
+    {
+    	return (User) context.getUserPrincipal();
     }
     
     @GET
