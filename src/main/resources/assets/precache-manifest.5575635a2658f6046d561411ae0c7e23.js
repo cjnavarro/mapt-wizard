4fb9fa8c39e17b2c4d9185f30fd50a3be7e565ac
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-23 10:34:28 -0400

    v1.0.1

---
+++ src/main/resources/assets/precache-manifest.5575635a2658f6046d561411ae0c7e23.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4235b39948d401d4f0e3af655986454e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca369237a76a86118a9",
+    "url": "/static/css/2.56d85521.chunk.css"
+  },
+  {
+    "revision": "8ba53d938ddef130dcb5",
+    "url": "/static/css/main.0557118d.chunk.css"
+  },
+  {
+    "revision": "5ca369237a76a86118a9",
+    "url": "/static/js/2.aa7374aa.chunk.js"
+  },
+  {
+    "revision": "8ba53d938ddef130dcb5",
+    "url": "/static/js/main.f373d0c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27e8e879248e8f2da86baffc53965715",
+    "url": "/static/media/aws.27e8e879.png"
+  },
+  {
+    "revision": "16f84931f129eb0992747c82d75d88c8",
+    "url": "/static/media/dropwizard.16f84931.png"
+  },
+  {
+    "revision": "8a85e6d62eb7e79168b2b96df11daa92",
+    "url": "/static/media/example.8a85e6d6.pdf"
+  },
+  {
+    "revision": "07c28adf28e0592b46094aef81fcc655",
+    "url": "/static/media/mysql.07c28adf.png"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/static/media/react.c3ba82a5.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/redux.8251fe76.png"
+  }
+]);
--- src/main/resources/assets/service-worker.js
+++ src/main/resources/assets/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bcaef7cc6a58f0765c43d54ec1743c2.js"
+  "/precache-manifest.5575635a2658f6046d561411ae0c7e23.js"
 );
 
 self.addEventListener('message', (event) => {
